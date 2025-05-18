05497825e455c95a4e59de28060dcc29d9743da1
Pablo <ppm998@alumnos.unican.es>
2025-05-18 10:59:16 +0200

    Actualización para conexión con backend

--- ski-shop/src/App.vue
+++ ski-shop/src/App.vue
@@ -13,19 +13,76 @@
 </template>
 
 <script>
-import {reactive} from 'vue'
 import ProductoItem from './components/ProductoItem.vue'
-import {productos} from './store/productos'
+import {reactive, onMounted } from 'vue';
 
 export default {
   components: { ProductoItem },
   setup() {
-    const modificarStock = (index, cantidad) => {
-      productos[index].stock += cantidad;
+    const productos = reactive([]);
+
+    const cargarProductos = async () => {
+      const res = await fetch(import.meta.env.VITE_API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query:`
+            {
+              todosProductos {
+                id
+                nombre
+                precio
+                stock
+                disponible
+              }
+            }
+          `
+        })
+      });
+      const { data, errors } = await res.json();
+      console.log("Respuesta recibida", data, errors);
+
+      if (data && data.todosProductos) {
+        productos.splice(0, productos.length, ...data.todosProductos);
+        
+      } else {
+        console.error("Error al obtener productos desde el backend:", errors)
+      }
     };
+
+    const modificarStock = async (index, cantidad) => {
+      const id = productos[index].id;
+      const res = await fetch(import.meta.env.VITE_API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              modificarStock(id: ${id}, cantidad: ${cantidad}) {
+                producto {
+                  id
+                  nombre
+                  precio
+                  stock
+                  disponible
+                }
+              }
+            }
+          `
+        })
+      });
+      const result = await res.json();
+      if (result.data) {
+        productos[index] = result.data.modificarStock.producto;
+      }
+    };
+
+    onMounted(() => {
+      cargarProductos();
+    });
     return { productos, modificarStock };
+
   }
 };
-
 </script>
 
